e5de76eb42e02ec3ee75e96bae7bb603337da0db
vtran91 <vtran16@bellsouth.net>
2023-02-13 20:49:46 +0000

    Logic for user/pass

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,10 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if ((username == "admin" && password == "qwerty") ||
+            (username == "user" && password == "password"))
+            return true;
+
         return false;
     }
 }
